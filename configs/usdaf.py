d8b7b58810ee9cd1bd9777d793b4d7a1b8c8e182
Kilter <mateodaza@gmail.com>
2025-08-04 11:20:47 -0500

    updaing branches for v2

--- configs/usdaf.py
+++ configs/usdaf.py
@@ -1,37 +1,29 @@
 topic0 = "0x962110f281c1213763cd97a546b337b3cbfd25a31ea9723e9d8b7376ba45da1a"
 
 branches = {
-    "scrvUSD": {
-        "trove_manager": "0xa0290af48d2E43162A1a05Ab9d01a4ca3a8B60CB",
-        "price_feed": "0x629b6c0DcDf865584FD58a08727ABb9Db7390e28",
+    "ysyBold": {
+        "trove_manager": "0xf8a25a2e4c863bb7cea7e4b4eeb3866bb7f11718",
+        "price_feed": "0x7f575323ddedfbad449fef5459fad031fe49520b",
     },
-    "sDAI": {
-        "trove_manager": "0x7F1171686e6028c321517EdB6DD70321164b6343",
-        "price_feed": "0xC470A1574B469A562fb237e289FDb217f8C14dc9",
+    "scrvUSD": {
+        "trove_manager": "0x7aff0173e3d7c5416d8caa3433871ef07568220d",
+        "price_feed": "0xf125c72ae447efdf3fa3601eda9ac0ebec06cbb8",
     },
     "sUSDS": {
-        "trove_manager": "0x2ba8e31b6C1C9f46046315406E840dBabeA803a8",
-        "price_feed": "0x806B2921E394b3f84A549AB89CF73e57F0C882c5",
+        "trove_manager": "0x53ce82ac43660aab1f80fecd1d74afe7a033d505",
+        "price_feed": "0x2113468843cf2d0fd976690f4ec6e4213df46911",
     },
     "sfrxUSD": {
-        "trove_manager": "0x53A5DE1b94d7409F75FFF49fd81A823fb874BF71",
-        "price_feed": "0xcDA8ccA990afF26fD8298e0d30304E4d01F7B387",
-    },
-    "sUSDe": {
-        "trove_manager": "0x9dc845b500853F17E238C36Ba120400dBEa1D02A",
-        "price_feed": "0x0DAaFdDcf74451caec724Bcd2f0d7E4025C95B94",
+        "trove_manager": "0x478e7c27193aca052964c3306d193446027630b0",
+        "price_feed": "0x653df748bf7a692555dcdbf4c504a8c84807f7c7",
     },
     "tBTC": {
-        "trove_manager": "0x64454C84Dc289C7CDe7E2eE2F87Ae1196bC9cD36",
-        "price_feed": "0xCe1Ca28e54fD3BD431F893DDFFFa1bd619C0517e",
+        "trove_manager": "0xfb17d0402ae557e3efa549812b95e931b2b63bce",
+        "price_feed": "0xeaf3b36748d89d64ef1b6b3e1d7637c3e4745094",
     },
-    "WBTC": {
-        "trove_manager": "0x085AbEe74F74E343647bdD2D68927e59163A0904",
-        "price_feed": "0x4d349971C23d6142e8dE9dEbbfdBB045B7AAbA49",
-    },
-    "cbBTC": {
-        "trove_manager": "0x0291C873838F7B62D743952D268BEbe9ace1efa4",
-        "price_feed": "0xAF99E6Cf5832222C0E22eF6bf0868C4Ed7f2953F",
+    "wBTC": {
+        "trove_manager": "0x7bd47eca45ee18609d3d64ba683ce488ca9320a3",
+        "price_feed": "0x4b74d043336678d2f62dae6595bc42dccabc3bb1",
     },
 }
 
